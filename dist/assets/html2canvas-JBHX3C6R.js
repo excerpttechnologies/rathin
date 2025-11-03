df996167ed91a59e40131413f9714ddf90146cc7
Aravind <aravindbvadavalliki@gmail.com>
2025-11-03 15:42:08 +0530

    saddcsx

--- dist/assets/html2canvas-JBHX3C6R.js
+++ dist/assets/html2canvas-JBHX3C6R.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-BzwXfTJ_.js";var t=e(((e,t)=>{
+import{n as e}from"./index-BKRFNgxu.js";var t=e(((e,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
