6d0dbee9872353ccdef9f88c3943308220e5697d
Aravind <aravindbvadavalliki@gmail.com>
2025-11-06 18:20:10 +0530

    dchbsdc

--- dist/assets/html2canvas-Ch9bsDQi.js
+++ dist/assets/html2canvas-Ch9bsDQi.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-Cm7zUstB.js";var t=e(((e,t)=>{
+import{n as e}from"./index-CMxZbl3G.js";var t=e(((e,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
