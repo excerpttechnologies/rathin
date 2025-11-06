81a3d4c0f06f599dce65dbb4e242daa463a4c637
Aravind <aravindbvadavalliki@gmail.com>
2025-11-06 14:26:47 +0530

    sfrve

--- dist/assets/html2canvas-BvfH8F0h.js
+++ dist/assets/html2canvas-BvfH8F0h.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-U45yhneI.js";var t=e(((e,t)=>{
+import{n as e}from"./index-C_H5vuHh.js";var t=e(((e,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
